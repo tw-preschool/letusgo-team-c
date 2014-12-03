af71d8c46725f736b1dd4b8dd0b436baa34a9bb1
zhangxv <raingxm@163.com>
2014-12-03 11:08:53 +0800

    [story 006] [niuwanlu&zhangxv] Admin Page, add two link(eidt and add) to show the new layer, which can edit product or add a new product

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	$('#item-table').find('.edit-product').on('click', function(event) {
-		console.log('edit product this time');
+		event.preventDefault();
+		openEditLayer();
+	});
+
+	$('.add-product').on('click', function(event) {
+		event.preventDefault();
+		openAddLayer();
 	});
 
 	$('#item-table').find('.delete-product').on('click', function(event) {
@@ -14,4 +20,22 @@ $(document).ready(function() {
 			data: {"name": productName}
 		});
 	});
+
+	function openEditLayer() {
+		$('.overlay').find('#submit').text('更新');
+		$('.overlay').fadeIn();
+		$('.overlay').find('#cancel').on('click', function(event) {
+			event.preventDefault();
+			$('.overlay').fadeOut();
+		});
+	}
+
+	function openAddLayer() {
+		$('.overlay').find('#submit').text('添加');
+		$('.overlay').fadeIn();
+		$('.overlay').find('#cancel').on('click', function(event) {
+			event.preventDefault();
+			$('.overlay').fadeOut();
+		});
+	}
 });
