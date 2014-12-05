850172df423ac6d3e8df4db62671f7083a4e938f
zhangxv <raingxm@163.com>
2014-12-05 21:50:58 +0800

    zhangxv - Database run setup.sh to create both products and items table

---
+++ db/2014_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration
+    def change
+        create_table :items do |t|
+            t.string :name
+            t.float :price
+            t.string :unit
+            t.integer :num
+            t.float :total_money
+            t.string :promoted
+            t.float :saving_money
+            t.timestamps 
+        end
+    end
+end
