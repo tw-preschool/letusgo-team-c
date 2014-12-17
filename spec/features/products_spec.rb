dcac96740b2d416edb0e28c91ed7c527318e6da7
zhangxv <raingxm@163.com>
2014-12-17 12:47:02 +0800

    [zhangxv] Add test to test post /products request can add new product in database

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -13,4 +13,19 @@ describe 'Products Application' do
     	expect(products.length).to eq 0
     end
   end
+
+  context 'add new product' do
+  	let(:body) { {:name => "Mac Book Pro", :price => 13456.89, :unit => "台"} }
+  	before { post 'products', body, {'Content-Type' => 'application/json'} }
+
+  	it 'insert into database properly' do 
+  		expect(last_response.status).to eq 201
+  	end
+
+  	it 'can find in database' do
+  		Product.all.last.name.should == "Mac Book Pro"
+  		Product.all.last.price.should == 13456.89
+  		Product.all.last.unit.should == "台"
+  	end
+  end
 end
