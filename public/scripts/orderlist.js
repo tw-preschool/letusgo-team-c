ee591d1eaa3b736ad31117cad19d8cc091b83a85
zhangxv <raingxm@163.com>
2014-12-24 22:09:25 +0800

    [zhangxv] OrderlistPage, change this page's header and footer to be admin_header and admin_footer

--- public/scripts/orderlist.js
+++ public/scripts/orderlist.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    changeNavHighlight();
+    addRemoveOrderHandler();
+});
+
+function changeNavHighlight() {
+    if(window.location.pathname == '/orders') {
+        $('.product-manage').removeClass('active');
+        $('.order-manage').addClass('active');
+    } else {
+        $('.product-manage').addClass('active');
+        $('.order-manage').removeClass('active');
+    }
+}
+
+function addRemoveOrderHandler() {
     $("[name='remove']").click(function() {
         var guid = $(this).attr("id");
         $(this).parent().parent().remove();
@@ -13,6 +28,4 @@ $(document).ready(function() {
             }
         });
     });
-
-
-});
+}
